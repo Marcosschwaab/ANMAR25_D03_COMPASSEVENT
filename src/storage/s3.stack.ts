d995e8c61cdb1c611ad92ed9358257ad3bdb0191
Marcos Schwaab <marcosschwaab@MacBook-Air-de-Marcos.local>
2025-06-03 13:52:29 -0300

    feat(lambda): create lambda and add to deploy s3.stack

--- src/storage/s3.stack.ts
+++ src/storage/s3.stack.ts
@@ -1,45 +1,108 @@
 import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
+import * as path from 'path';
 
 export class S3BucketStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const s3BucketName = process.env.S3_BUCKET_NAME_CDK || `user-profiles-nest-${this.account}-${this.region}`;
+    const s3BucketName = process.env.S3_BUCKET_NAME || `user-profiles-compass-${this.account}-${this.region}`;
 
-    const userProfilesBucket = new s3.Bucket(this, 'UserProfilesNestBucket', {
+    const imageResizerLambda = new lambda.Function(this, 'ImageResizerLambda', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../../lambda.zip')),
+      memorySize: 512,
+      timeout: cdk.Duration.seconds(30),
+      initialPolicy: [
+        new cdk.aws_iam.PolicyStatement({
+          actions: ['s3:GetObject', 's3:PutObject'],
+          resources: [`arn:aws:s3:::${s3BucketName}/*`],
+        }),
+      ],
+    });
+
+    const userProfilesCfnBucket = new s3.CfnBucket(this, 'UserProfilesNestCfnBucket', {
       bucketName: s3BucketName,
-      publicReadAccess: true,
-      blockPublicAccess: new s3.BlockPublicAccess({
+      publicAccessBlockConfiguration: {
         blockPublicAcls: false,
         ignorePublicAcls: false,
         blockPublicPolicy: false,
         restrictPublicBuckets: false,
-      }),
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-      cors: [
-        {
-          allowedMethods: [
-            s3.HttpMethods.GET,
-            s3.HttpMethods.POST,
-            s3.HttpMethods.PUT,
-            s3.HttpMethods.DELETE,
-            s3.HttpMethods.HEAD,
-          ],
-          allowedOrigins: ['*'],
-          allowedHeaders: ['*'],
-          exposedHeaders: [],
-        },
-      ],
-      encryption: s3.BucketEncryption.S3_MANAGED,
-      versioned: false,
+      },
+      corsConfiguration: {
+        corsRules: [
+          {
+            allowedMethods: ['GET', 'POST', 'PUT', 'DELETE', 'HEAD'],
+            allowedOrigins: ['*'],
+            allowedHeaders: ['*'],
+          },
+        ],
+      },
+      tags: [{ key: 'Name', value: s3BucketName }],
     });
 
+    userProfilesCfnBucket.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
+
+    // Create a CfnBucketPolicy to allow public read access to objects
+    new s3.CfnBucketPolicy(this, 'UserProfilesBucketPolicy', {
+      bucket: userProfilesCfnBucket.bucketName!, // Reference the bucket by its name
+      policyDocument: {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Effect: 'Allow',
+            Principal: '*', // Allow access to anyone
+            Action: 's3:GetObject', // Allow GetObject action
+            // Correctly construct the ARN for objects within the CfnBucket
+            Resource: cdk.Fn.join('', [userProfilesCfnBucket.attrArn, '/*']),
+          },
+        ],
+      },
+    });
+
+    const lambdaConfigurations: s3.CfnBucket.LambdaConfigurationProperty[] = [];
+    const prefixes = ['profiles/', 'events/'];
+    const suffixes = ['.jpg', '.jpeg', '.png'];
+
+    prefixes.forEach(prefix => {
+      suffixes.forEach(suffix => {
+        lambdaConfigurations.push({
+          event: 's3:ObjectCreated:*',
+          function: imageResizerLambda.functionArn,
+          filter: {
+            s3Key: {
+              rules: [
+                { name: 'prefix', value: prefix },
+                { name: 'suffix', value: suffix }
+              ]
+            }
+          }
+        });
+
+        new lambda.CfnPermission(this, `S3InvokeLambdaPermission-${prefix.replace('/', '')}-${suffix.replace('.', '')}`, {
+          action: 'lambda:InvokeFunction',
+          functionName: imageResizerLambda.functionName,
+          principal: 's3.amazonaws.com',
+          sourceArn: `arn:aws:s3:::${s3BucketName}`,
+          sourceAccount: cdk.Stack.of(this).account,
+        });
+      });
+    });
+
+    userProfilesCfnBucket.notificationConfiguration = {
+      lambdaConfigurations: lambdaConfigurations,
+    };
+
+    userProfilesCfnBucket.node.addDependency(imageResizerLambda);
+
     new cdk.CfnOutput(this, 'BucketNameOutput', {
-      value: userProfilesBucket.bucketName,
-      description: 'Name of the S3 bucket for user profiles',
+      value: userProfilesCfnBucket.bucketName!,
+      description: 'Name of the S3 bucket for user profiles and event images',
       exportName: 'UserProfilesNestBucketName',
     });
   }
